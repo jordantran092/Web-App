486249e77e2a8b02927fdcbf3543665572e1bd68
tran-jordan <77415360+jordantran092@users.noreply.github.com>
2026-08-08 18:19:18 -0400

    a

--- app/pages/[id]/page.tsx
+++ app/pages/[id]/page.tsx
@@ -1,7 +1,10 @@
 
 import * as PageActions from "@/actions/PageActions";
 import Page from "@/components/Page";
+import { auth } from "@/lib/auth";
 import { Block } from "@blocknote/core/blocks";
+import { headers } from "next/headers";
+import { notFound } from "next/navigation";
 
 
 type PagesProps = {
@@ -11,28 +14,57 @@ type PagesProps = {
 
 export default async function Pages({ params } : PagesProps) {
     
-    
 
     const {id} = await params;
-
-
-    /* Loading saved blocks */
     const page = await PageActions.getPage(id);
 
-    let initialContent: Block[] = [];
 
     if(page) {
-        const blocks = page.blocks;
-        // need initialContent prop as a Block[]
-        initialContent = JSON.parse(blocks) as Block[];
+    
+        
+        /* Auth Check to see if user owns this page */
+        const session = await auth.api.getSession({
+            headers: await headers(),
+        });
+
+
+        if(session) {
+
+            const userId = session.user.id;
+    
+            if(page.userId == userId) {
+
+                /* Loading saved blocks */
+            
+        
+                let initialContent: Block[] = [];
+        
+                
+                const blocks = page.blocks; //fixme SADF82349823984
+                // need initialContent prop as a Block[]
+                initialContent = JSON.parse(blocks) as Block[];
+            
+        
+        
+        
+                return (
+                    <>
+                        <Page id={id} initialContent={initialContent}/>
+                    </>
+                    
+                );
+            }
+    
+        }
+
     }
 
 
-
+    
     return (
-        <>
-            <Page id={id} initialContent={initialContent}/>
-        </>
-        
+        notFound()
     );
+
+
+   
 }
