602d0ddbfd4f689a43ac7cb0b5bb349426f359cf
tran-jordan <jordantran092@gmail.com>
2026-08-10 15:41:12 -0400

    a

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -8,15 +8,24 @@ import '@blocknote/core/fonts/inter.css';
 import { Block, createExtension } from '@blocknote/core';
 import { PageUpdateInput } from '@/types/Page';
 import { updatePage } from '@/actions/PageActions';
-import '@/styles.css';
 
 type EditorProps = {
     id: string;
     initialContent?: Block<any, any, any>[];
     setIsSaving: (value: boolean) => void;
+    isSaving: boolean;
+    isSavingTimerOn: boolean;
+    setIsSavingTimerOn: (value: boolean) => void;
 };
 
-export default function Editor({ id, initialContent, setIsSaving }: EditorProps) {
+export default function Editor({
+    id,
+    initialContent,
+    setIsSaving,
+    isSaving,
+    isSavingTimerOn,
+    setIsSavingTimerOn,
+}: EditorProps) {
     // Create a new editor instance
     const editor = useCreateBlockNote({
         initialContent,
@@ -29,24 +38,31 @@ export default function Editor({ id, initialContent, setIsSaving }: EditorProps)
                 // expects an object of a key value pair, key is the shortcut, value is the arrow function
                 keyboardShortcuts: {
                     'Mod-s': ({ editor }) => {
-                        //debug
-                        // console.log("Saved page");
+                        // do not allow saving if already in process of saving to avoid any desync of values
+                        if (!(isSaving || isSavingTimerOn)) {
+                            const savedBlocks = JSON.stringify(editor.document);
 
-                        const savedBlocks = JSON.stringify(editor.document);
+                            const pageEntity: PageUpdateInput = {
+                                id: id,
+                                blocks: savedBlocks,
+                            };
 
-                        const pageEntity: PageUpdateInput = {
-                            id: id,
-                            blocks: savedBlocks,
-                        };
+                            console.log('timer: ' + isSavingTimerOn);
+                            setIsSaving(true);
+                            setIsSavingTimerOn(true);
+                            updatePage(pageEntity)
+                                .then((_) => {
+                                    setIsSaving(false);
+                                })
+                                .catch((error) => {
+                                    console.log('Error: Page updated failed. ' + error);
+                                });
 
-                        setIsSaving(true);
-                        updatePage(pageEntity)
-                            .then((_) => {
-                                setIsSaving(false);
-                            })
-                            .catch((error) => {
-                                console.log('Error: Page updated failed. ' + error);
-                            });
+                            console.log('saving!');
+                        } else {
+                            // debug
+                            console.log('already saving!');
+                        }
 
                         return true; // tell BlockNote the shortcut was handled, so that browser does not try to handle it with default response
                     },
---
+++ components/SavingIndicator.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { useEffect } from 'react';
+import LoadingSpinner from './LoadingSpinner';
+
+type SavingIndicatorProps = {
+    setIsSavingTimerOn: (value: boolean) => void;
+};
+
+export default function SavingIndicator({ setIsSavingTimerOn }: SavingIndicatorProps) {
+    useEffect(() => {
+        // Set a timer for 1 second
+        const timer = setTimeout(() => {
+            setIsSavingTimerOn(false);
+        }, 5000);
+
+        // Cleanup function to clear the timer if component unmounts
+        return () => clearTimeout(timer);
+    }, []); // Empty array ensures this runs only once on mount
+
+    return (
+        <>
+            {/* flex to align items inline, and gap to give some space between the 2 items */}
+            <div className="flex gap-2">
+                <div className="mt-1">
+                    <LoadingSpinner />
+                </div>
+                <div className="mt-1">
+                    <p className="text-sm">Saving...</p>
+                </div>
+            </div>
+        </>
+    );
+}
