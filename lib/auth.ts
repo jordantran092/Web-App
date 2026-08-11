f20c9c217219da3937cf58f0e02b0fdd3ad44204
tran-jordan <jordantran092@gmail.com>
2026-08-11 17:59:06 -0400

    a

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,17 +1,19 @@
-import { betterAuth } from "better-auth";
-import { prismaAdapter } from "better-auth/adapters/prisma";
-import { prisma } from "@/lib/prisma"; // single prisma client you generated
-import { nextCookies } from "better-auth/next-js";
+import { betterAuth } from 'better-auth';
+import { prismaAdapter } from 'better-auth/adapters/prisma';
+import { prisma } from '@/lib/prisma'; // single prisma client you generated
+import { nextCookies } from 'better-auth/next-js';
 
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
-        provider: "sqlite", 
+        provider: 'sqlite',
     }),
 
     // our chosen auth option
-    emailAndPassword: { 
-        enabled: true, 
-    }, 
+    emailAndPassword: {
+        enabled: true,
+    },
 
-    plugins: [nextCookies()] // this makes sure cookies are being updated properly when doing auth. also make sure this is the last plugin in the array
+    plugins: [nextCookies()], // this makes sure cookies are being updated properly when doing auth. also make sure this is the last plugin in the array
 });
+
+export type Session = typeof auth.$Infer.Session;
--- services/PageService.ts
+++ services/PageService.ts
@@ -1,27 +1,51 @@
-"use server";
+'use server';
 
+import { Session } from '@/lib/auth';
+import { prisma } from '@/lib/prisma'; // single prisma client generated from the prisma.ts file
+import { PageUpdateInput } from '@/types/Page';
+import { Block } from '@blocknote/core/blocks';
 
-import { prisma } from "@/lib/prisma"; // single prisma client generated from the prisma.ts file
-import { PageUpdateInput } from "@/types/Page";
-
-
-
-export async function updatePage({ id, ...data } : PageUpdateInput) {
-    
+export async function updatePage({ id, ...data }: PageUpdateInput) {
     await prisma.page.update({
-        where: { id }, 
+        where: { id },
 
-        data // since data param is same as data property name, use property shorthand as so. will also only comprise of fields entered, optional fields ignored
-
+        data, // since data param is same as data property name, use property shorthand as so. will also only comprise of fields entered, optional fields ignored
     });
 }
 
-
 export async function getPage(id: string) {
-    
     // async function will wrap return in a promise again, so await here will be useless
     return prisma.page.findUnique({
-        where: { id }, 
-
+        where: { id },
     });
+}
+
+export async function getContentOfPage(session: Session, id: string) {
+    const page = await getPage(id);
+
+    if (!page) {
+        throw new Error('NOT_FOUND');
+    }
+
+    /* Auth Check to see if user owns this page */
+
+    const userId = session.user.id;
+
+    let initialContent: Block[] = [];
+
+    if (page.userId == userId) {
+        console.log('authorized');
+        // User is authorized
+
+        /* Loading saved blocks */
+
+        const blocks = page.blocks;
+
+        // need initialContent prop as a Block[]. Null checking to avoid JSON parse gives error
+        if (blocks) initialContent = JSON.parse(blocks) as Block[];
+    } else {
+        throw new Error('UNAUTHORIZED');
+    }
+
+    return initialContent;
 }
