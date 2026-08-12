54f9741e85638170db5c692aacef90af1f411499
tran-jordan <jordantran092@gmail.com>
2026-08-12 14:35:04 -0400

    a

--- hooks/useAuth.tsx
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-import { Keycloak } from "keycloak-js";npm 
-
-export default function useAuth() {
-    const [isLogin, setIsLogin] = useState(false);
-
-    const client = new Keycloak();
-}
