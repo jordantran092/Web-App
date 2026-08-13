5782d8a328441b36fb3512c49f12f02bb728b2e7
tran-jordan <jordantran092@gmail.com>
2026-08-13 10:53:53 -0400

    a

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,24 +1,23 @@
-import * as AuthActions from "@/actions/AuthActions";
-
-
-
+import * as AuthActions from '@/actions/AuthActions';
+import { Button } from '@/components/ui/button';
 
 export default function SignInPage() {
-    
-
-
     return (
-        <form action={AuthActions.signIn}>
-            <label>Enter your email:
-                <input type="text" name="email" required />
-            </label>
-            <br/>
-            <label>Enter your password:
-                <input type="password" name="password" required />
-            </label>
-            <br/>
-            <br/>
-            <button>Submit</button>
-        </form>
+        <>
+            <form action={AuthActions.signIn}>
+                <label>
+                    Enter your email:
+                    <input type="text" name="email" required />
+                </label>
+                <br />
+                <label>
+                    Enter your password:
+                    <input type="password" name="password" required />
+                </label>
+                <br />
+                <br />
+                <button>Submit</button>
+            </form>
+        </>
     );
 }
