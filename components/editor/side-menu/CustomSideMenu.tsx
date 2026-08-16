5e10156c769296fa4936bfbc299d19a0c02b71c8
tran-jordan <jordantran092@gmail.com>
2026-08-16 14:41:00 -0400

    a

---
+++ components/editor/side-menu/CustomSideMenu.tsx
@@ -0,0 +1,23 @@
+import {
+    BlockColorsItem,
+    DragHandleMenu,
+    RemoveBlockItem,
+    SideMenu,
+    SideMenuProps,
+} from '@blocknote/react';
+import { MoveToItem } from './MoveToItem';
+
+export const CustomSideMenu = (props: SideMenuProps) => (
+    <SideMenu {...props} dragHandleMenu={CustomDragHandleMenu} />
+);
+
+// To avoid rendering issues, it's good practice to define your custom drag
+// handle menu in a separate component, instead of inline within the `sideMenu`
+// prop of `SideMenuController`.
+const CustomDragHandleMenu = () => (
+    <DragHandleMenu>
+        <BlockColorsItem>Color</BlockColorsItem>
+        <MoveToItem>Move To</MoveToItem> {/* Custom Item */}
+        <RemoveBlockItem>Delete</RemoveBlockItem>
+    </DragHandleMenu>
+);
---
+++ components/editor/side-menu/MoveToItem.tsx
@@ -0,0 +1,28 @@
+import {} from '@blocknote/core';
+import { SideMenuExtension } from '@blocknote/core/extensions';
+import { useBlockNoteEditor, useComponentsContext, useExtensionState } from '@blocknote/react';
+import { ReactNode } from 'react';
+
+export function MoveToItem(props: { children: ReactNode }) {
+    const editor = useBlockNoteEditor();
+
+    const Components = useComponentsContext()!;
+
+    const block = useExtensionState(SideMenuExtension, {
+        selector: (state) => state?.block,
+    });
+
+    if (!block) {
+        return null;
+    }
+
+    return (
+        <Components.Generic.Menu.Item
+            onClick={() => {
+                // editor.updateBlock(block, { type: 'paragraph' });
+                console.log('hello');
+            }}>
+            {props.children}
+        </Components.Generic.Menu.Item>
+    );
+}
