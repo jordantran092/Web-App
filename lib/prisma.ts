6ae6411dc819b894296937b11d3e84c87a0e8631
tran-jordan <jordantran092@gmail.com>
2026-08-27 09:19:19 -0400

    a

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,17 +1,18 @@
 // lib/prisma.ts
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
-import { PrismaClient } from "@/app/generated/prisma/client"; // default path
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@/app/generated/prisma/client'; // default path
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 const databaseUrl = process.env.DATABASE_URL;
 
 if (!databaseUrl) {
-  throw new Error("DATABASE_URL is not set");
+    throw new Error('DATABASE_URL is not set');
 }
 
-const adapter = new PrismaBetterSqlite3({ url: databaseUrl });
-export const prisma = globalForPrisma.prisma || new PrismaClient({adapter: adapter}); // passing in object that has property adapter using your adapter var
-   
+const adapter = new PrismaPg({
+    connectionString: databaseUrl,
+});
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter: adapter }); // passing in object that has property adapter using your adapter var
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
