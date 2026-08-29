e0717443245b18273eaf5ec97a115fc1b78c5cd7
tran-jordan <jordantran092@gmail.com>
2026-08-29 15:16:12 -0400

    a

--- components/sidebar/FTSMenu.tsx
+++ components/sidebar/FTSMenu.tsx
@@ -27,6 +27,8 @@ import {
 import LoadingSpinner from '../LoadingSpinner';
 import { StyledText } from '@blocknote/core';
 import { getText } from '@/utils/block-utils';
+import { Input } from '../ui/input';
+import { SearchIcon } from 'lucide-react';
 
 type FTSMenuProps = {
     isFTSMenuOpen: boolean;
@@ -54,9 +56,11 @@ export default function FTSMenu({ isFTSMenuOpen, setIsFTSMenuOpen }: FTSMenuProp
             setLoading(false);
         }
 
-        // Only pull pages when search input changes
-        if (search) {
+        // Only pull pages when search input changes, and do not fetch when ''
+        if (search !== '') {
             getItems();
+        } else {
+            setItems([]);
         }
     }, [search]);
 
@@ -68,26 +72,39 @@ export default function FTSMenu({ isFTSMenuOpen, setIsFTSMenuOpen }: FTSMenuProp
             <CommandItem
                 key={index}
                 value={title + index} // Has to be unique values for each CommandItem or else, run into styling conflicts
-                onSelect={() => {
-                    //router push new tab
-                }}>
+                // onSelect={() => {
+                //     //router push new tab
+                // }}
+            >
                 <HiDocumentText size={23} />
                 <span>{title}</span>
             </CommandItem>
         );
     });
 
+    // console.log(items);
+    console.log(cmdItemsArr);
+
     return (
         <div className="flex flex-col gap-4">
             <CommandDialog open={isFTSMenuOpen} onOpenChange={setIsFTSMenuOpen}>
                 <Command>
-                    <CommandInput
-                        placeholder="Search..."
-                        value={search}
-                        onValueChange={setSearch}
-                    />
+                    <div className="mx-1 mt-1 flex h-8 min-w-0 gap-2 rounded-lg border border-input bg-transparent px-2.5 py-1 text-base transition-colors outline-none file:inline-flex file:h-6 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground disabled:pointer-events-none disabled:cursor-not-allowed disabled:bg-input/50 disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 md:text-sm dark:bg-input/30 dark:disabled:bg-input/80 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40">
+                        <SearchIcon className="mt-0.5 size-4 shrink-0 opacity-50" />
+                        <input
+                            // removes default CSS outline when focus/click on input
+                            className="w-full focus:outline-none"
+                            placeholder="Search..."
+
+                            value={search}
+                            onChange={(e) => {
+                                setSearch(e.currentTarget.value);
+                            }}
+                        />
+                    </div>
+
                     <CommandList>
-                        <CommandEmpty>No results found.</CommandEmpty>
+                        <CommandEmpty>No results found. </CommandEmpty>
 
                         <CommandGroup heading="Pages">
                             {loading && (
@@ -95,7 +112,6 @@ export default function FTSMenu({ isFTSMenuOpen, setIsFTSMenuOpen }: FTSMenuProp
                                     <LoadingSpinner />
                                 </CommandLoading>
                             )}
-
                             {cmdItemsArr}
                         </CommandGroup>
                     </CommandList>
